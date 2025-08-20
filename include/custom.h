a9840d4a2825bed3640786bbac997413e530af19
jonghyun <dkrehd0519@naver.com>
2025-08-20 08:57:29 +0000

    custom file setting

--- include/custom.h
+++ include/custom.h
@@ -18,3 +18,36 @@ header 파일 분리
 
 마운트시 서버의 어떤 디렉토리와 연동할지 옵션을 주든지 해서
 */
+
+#define FUSE_USE_VERSION 31
+
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <assert.h>
+
+/*
+ * Command line options
+ *
+ * We can't set default values for the char* fields here because
+ * fuse_opt_parse would attempt to free() them when the user specifies
+ * different values on the command line.
+ */
+static struct options {
+	const char *filename;
+	const char *contents;
+	int show_help;
+} options;
+
+#define OPTION(t, p)                           \
+    { t, offsetof(struct options, p), 1 }
+static const struct fuse_opt option_spec[] = {
+	OPTION("--name=%s", filename),
+	OPTION("--contents=%s", contents),
+	OPTION("-h", show_help),
+	OPTION("--help", show_help),
+	FUSE_OPT_END
+};
