709b0d7d0745e433773289cf568863d7e58ae866
kanghyeonLee <lkhlkh0823@handong.ac.kr>
2025-08-22 04:00:45 +0900

    asdasf

--- source/fuse_study.c
+++ source/fuse_study.c
@@ -394,9 +394,9 @@ void fuse_study_readdir(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off,
     bound_send(serv_sd, pkt_data, &opcode, sizeof(int));
     char* path_to_send;
     if (ino == FUSE_ROOT_ID) {
-            path_to_send = "./"; // 루트 아이노드일 경우, 경로를 "/"로 지정
+            path_to_send = "./";
         } else {
-         path_to_send = _path; // 그 외에는 기존 방식 사용 (여전히 문제는 있음)
+         path_to_send = _path;
     }
     bound_send(serv_sd, pkt_data, path_to_send, strlen(path_to_send)+1);
     
