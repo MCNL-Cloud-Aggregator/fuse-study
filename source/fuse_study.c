b8b587cf92bf0ba5ea3fa61e4f921395dc6f569a
ymail8372 <hs1261101@gmail.com>
2025-08-21 14:44:31 +0000

    fuse_study.c add fuse_study_read, fuse_study_write

--- source/fuse_study.c
+++ source/fuse_study.c
@@ -93,6 +93,44 @@ void error_handling(char* message) {
 }
 
 
+void fuse_study_read (fuse_req_t req, fuse_ino_t ino, size_t size, off_t off, struct fuse_file_info *fi) {
+	
+	struct pkt send_buf, read_buf;
+	unsigned short opcode = READ;
+	char* data;
+	
+	bound_send(serv_sd, &send_buf, &opcode, sizeof(opcode));
+	bound_send(serv_sd, &send_buf, _path, strlen(_path)+1);
+	
+	size_t read_byte = 0;
+	do {
+		bound_read(serv_sd, &read_buf);
+		if (data == NULL) {
+			data = malloc(read_buf.total_size);
+		}
+		memset(&data[read_byte], read_buf.buf, read_buf.size);
+		
+		read_byte += read_buf.size;
+		if (read_buf.end == 1) {
+			break;
+		}
+	} while(true);
+    
+    fuse_reply_buf(req, data, read_byte);
+}
+
+void fuse_study_write (fuse_req_t req, fuse_ino_t ino, const char *buf, size_t size, off_t off, struct fuse_file_info *fi) {
+	
+	struct pkt send_buf, read_buf;
+	unsigned short opcode = READ;
+	char* data;
+	
+	bound_send(serv_sd, &send_buf, &opcode, sizeof(opcode));
+	bound_send(serv_sd, &send_buf, _path, strlen(_path));
+	
+	bound_send(serv_sd, &send_buf, buf, strlen(buf));
+	
+}
 
 void fuse_study_unlink(fuse_req_t req, fuse_ino_t parent, const char *path) {
     printf("unlink executed: parent=%lu, name=%s\n", parent, path);
--- source/ms/read.c
+++ source/ms/read.c
@@ -3,11 +3,25 @@
 
 void fuse_study_read (fuse_req_t req, fuse_ino_t ino, size_t size, off_t off, struct fuse_file_info *fi) {
 	
-	struct pkt send_buf;
+	struct pkt send_buf, read_buf;
 	unsigned short opcode = READ;
+	char* data;
 	
 	bound_send(serv_sd, &send_buf, &opcode, sizeof(opcode));
 	bound_send(serv_sd, &send_buf, _path, strlen(_path));
 	
+	size_t read_byte = 0;
+	do {
+		bound_read(serv_sd, read_buf);
+		if (data == NULL) {
+			data = malloc(read_buf.total_size);
+		}
+		memset(&data[read_byte], read_buf.buf, read_buf.size);
+		
+		read_byte += read_buf.size;
+		if (read_buf.end == 1) {
+			break;
+		}
+	}
 	
 }
